451b71a47f8b10e1471b6c4f076bd2abe2c55f4a
Peter Hanecak <hanecak@opendata.sk>
2018-04-07 14:58:54 +0200

    batch size changed from 10 to 100

--- datastore_updater.py
+++ datastore_updater.py
@@ -28,7 +28,7 @@ USAGE = '''
 
 #PAST_DAY_DATA_URL = 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson'
 #PAST_HOUR_DATA_URL = 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson'
-BATCH_SIZE = 10
+BATCH_SIZE = 100
 STATE_FILE = 'datastore_updater.state'
 
 # state keys
